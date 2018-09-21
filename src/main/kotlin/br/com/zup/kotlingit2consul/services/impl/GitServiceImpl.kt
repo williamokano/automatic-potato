983bdb151e2ff02508b57780c50da3a7ce7e0530
William Johnson dos Santos Okano <williamokano@gmail.com>
2018-09-21 19:32:35 -0300

    Reading data from git and from the file system

--- src/main/kotlin/br/com/zup/kotlingit2consul/services/impl/GitServiceImpl.kt
+++ src/main/kotlin/br/com/zup/kotlingit2consul/services/impl/GitServiceImpl.kt
@@ -2,6 +2,8 @@ package br.com.zup.kotlingit2consul.services.impl
 
 import br.com.zup.kotlingit2consul.services.GitService
 import br.com.zup.kotlingit2consul.utils.GitUtils
+import org.slf4j.LoggerFactory
+import java.io.File
 
 class GitServiceImpl(
     val keyPrefix: String,
@@ -14,18 +16,41 @@ class GitServiceImpl(
         "rw-router/data" to "something that sould be \${replaced}"
     )
 
+    private companion object {
+        val LOGGER = LoggerFactory.getLogger(this::class.java)!!
+    }
+
     override fun getKeys(): Set<String> {
-        return mockKv.keys.asSequence().map(this::buildKeys).toSet()
+        LOGGER.info("Getting info from $repositoryPath")
+        return getFolderContents(repositoryPath, keyPrefix.trim('/')).toSet()
     }
 
     override fun get(key: String): String = key.removePrefix(keyPrefix).trim().let { normalizedKey ->
-        mockKv.getOrElse(normalizedKey) {
-            throw RuntimeException("Key $normalizedKey do not exist")
-        }
+        val filePath = "$repositoryPath/$normalizedKey"
+        LOGGER.info("Getting value from $filePath")
+        File(filePath).readText()
     }
 
     override fun updateRepository() {
         GitUtils.pullOrClone(repositoryUrl, repositoryPath)
+    }
+
+    private fun getFolderContents(path: String, prepend: String = ""): List<String> {
+        val directory = File(path)
+        val directoryContents = directory.listFiles()
+        if (directoryContents != null) {
+            val files = directoryContents.filter { it.isFile && it.name.startsWith(".").not() }
+            val directories = directoryContents.filter { it.isDirectory && it.name.startsWith(".").not() }
+
+            val strFiles = files.map { "$prepend/${it.name}".trim('/') }
+            val strDirectories = directories.flatMap { getFolderContents(it.path, "$prepend/${it.name.trim('/')}") }
+
+            return mutableListOf<String>()
+                .apply { addAll(strFiles) }
+                .apply { addAll(strDirectories) }
+        }
+
+        return emptyList()
     }
 
     private fun buildKeys(key: String) =
